a5723ada05131d2338c75d93b9e6d7765c9ee956
sheldon <sheldonlau1@gmail.com>
2024-08-24 16:03:12 -0700

    generate blue to white gradient

--- main.go
+++ main.go
@@ -6,10 +6,44 @@ import (
 	"os"
 )
 
+func RayColor(r *Ray) *Color {
+	unitDirection := UnitVector(r.Dir())
+	a := 0.5 * (unitDirection.Y() + 1.0)
+	u := VectorScalarProduct(1.0-a, NewVec3(1.0, 1.0, 1.0))
+	v := VectorScalarProduct(a, NewVec3(0.5, 0.7, 1.0))
+	return VectorSum(u, v)
+}
+
 func main() {
-	// Image
-	imageWidth := 256
-	imageHeight := 256
+	aspectRatio := 16.0 / 9.0
+	imageWidth := 400
+
+	// Calculate the image height, and ensure that it's at least 1.
+	imageHeight := int(float64(imageWidth) / aspectRatio)
+	if imageHeight < 1 {
+		imageHeight = 1
+	}
+
+	// Camera
+
+	focalLength := 1.0
+	viewportHeight := 2.0
+	viewportWidth := viewportHeight * (float64(imageWidth) / float64(imageHeight))
+	cameraCenter := NewVec3(0, 0, 0)
+
+	// Calculate the vectors across the horizontal and down the vertical viewport edges.
+	viewportU := NewVec3(viewportWidth, 0, 0)
+	viewportV := NewVec3(0, -viewportHeight, 0)
+
+	// Calculate the horizontal and vertical delta vectors from pixel to pixel.
+	pixelDeltaU := VectorScalarDivision(float64(imageWidth), viewportU)
+	pixelDeltaV := VectorScalarDivision(float64(imageHeight), viewportV)
+
+	// Calculate the location of the upper left pixel.
+	vpUCenter := VectorScalarDivision(2, viewportU)
+	vpVCenter := VectorScalarDivision(2, viewportV)
+	viewportUpperLeft := VectorDiff(VectorDiff(VectorDiff(cameraCenter, NewVec3(0, 0, focalLength)), vpUCenter), vpVCenter)
+	pixel00Loc := VectorSum(viewportUpperLeft, VectorScalarProduct(0.5, VectorSum(pixelDeltaU, pixelDeltaV)))
 
 	// Render
 	fmt.Printf("P3\n%d %d\n255\n", imageWidth, imageHeight)
@@ -17,7 +51,13 @@ func main() {
 	for j := 0; j < imageHeight; j++ {
 		log.Printf("\rScanlines remaining: ------------------------------------- %d ", (imageHeight - j))
 		for i := 0; i < imageWidth; i++ {
-			pixelColor := NewVec3(float64(i)/float64(imageWidth-1), float64(j)/float64(imageHeight-1), 0)
+			deltaU := VectorScalarProduct(float64(i), pixelDeltaU)
+			deltaV := VectorScalarProduct(float64(j), pixelDeltaV)
+			offset := VectorSum(deltaU, deltaV)
+			pixelCenter := VectorSum(pixel00Loc, offset)
+			rayDirection := VectorSum(pixelCenter, cameraCenter.Negative())
+			r := Ray{cameraCenter, rayDirection}
+			pixelColor := RayColor(&r)
 			WriteColor(os.Stdout, pixelColor)
 		}
 	}
