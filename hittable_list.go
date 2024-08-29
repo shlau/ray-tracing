92f63484b84566a0de168260babfefef40ca63cc
sheldon <sheldonlau1@gmail.com>
2024-08-28 18:20:22 -0700

    add hittable struct

---
+++ hittable_list.go
@@ -0,0 +1,32 @@
+package main
+
+type HittableList struct {
+	objects []Hittable
+}
+
+func NewHittableList(objs []Hittable) *HittableList {
+	return &HittableList{objects: objs}
+}
+
+func (h *HittableList) clear() {
+	h.objects = nil
+}
+
+func (h *HittableList) add(obj Hittable) {
+	h.objects = append(h.objects, obj)
+}
+
+func (h *HittableList) Hit(r *Ray, rayTMin float64, rayTMax float64, rec *HitRecord) bool {
+	tempRec := &HitRecord{}
+	hitAnything := false
+	closestSoFar := rayTMax
+
+	for _, obj := range h.objects {
+		if obj.Hit(r, rayTMin, closestSoFar, tempRec) {
+			hitAnything = true
+			closestSoFar = tempRec.t
+			rec = tempRec
+		}
+	}
+	return hitAnything
+}
